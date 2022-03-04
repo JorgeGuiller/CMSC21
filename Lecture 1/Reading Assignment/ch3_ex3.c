1e0f3454a464e6d186896d9f5d6a8c8eceee6725
JorgeGuiller <100853100+JorgeGuiller@users.noreply.github.com>
2022-03-04 14:35:21 +0800

    Update ch3_ex3.c

--- Lecture 1/Reading Assignment/ch3_ex3.c
+++ Lecture 1/Reading Assignment/ch3_ex3.c
@@ -2,21 +2,21 @@
 
 int main(void) {
 
-    //  VARIABLE DECLARATIONS
+     // Variable Declarations for integer data type
     int num1, denom1, num2, denom2, result_num, result_denom;
 
-    // USER INPUTS
+    // Displays on terminal the printf and asks for a value from the User
     printf("Enter first fraction: ");
     scanf("%d/%d", &num1, &denom1);
 
     printf("Enter second fraction: ");
     scanf("%d/%d", &num2, &denom2);
 
-    /* LOGIC FOR ADDING FRACTIONS */
+    // Formula to add fractions
     result_num = num1 * denom2 + num2 * denom1;
     result_denom = denom1 * denom2;
 
-    // PRINT STATEMENT
+    // Final print statement showing the string then the values of the variable result_num and result_denom
     printf("The sum is %d/%d\n", result_num, result_denom);
 
     return 0;
