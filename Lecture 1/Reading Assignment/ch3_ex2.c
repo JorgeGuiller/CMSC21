286c6b68b51f1d65f7139b97304fe8826910296d
JorgeGuiller <100853100+JorgeGuiller@users.noreply.github.com>
2022-03-04 14:31:25 +0800

    Update ch3_ex2.c

--- Lecture 1/Reading Assignment/ch3_ex2.c
+++ Lecture 1/Reading Assignment/ch3_ex2.c
@@ -2,15 +2,16 @@
 
 int main(void) {
 
-    //  VARIABLE DECLARATIONS
+    // Variable Declarations for integer data type
     int i;
+    // Variable Declarations for float data type
     float x;
 
-    // VARIABLES
+    // Assigned Values to Variables
     i = 40;
     x = 839.21f;
 
-    //  PRINT STATEMENTS
+    //  Print Statement
     printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i);
     printf("|%10.3f|%10.3e|%-10g|\n", x, x, x);
 
