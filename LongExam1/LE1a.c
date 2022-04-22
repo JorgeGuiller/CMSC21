184e5dd66af3c1e8fb464f7feb99a8012c17837f
JorgeGuiller <100853100+JorgeGuiller@users.noreply.github.com>
2022-04-22 14:16:14 +0800

    Update LE1a.c

--- LongExam1/LE1a.c
+++ LongExam1/LE1a.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 void main()
 {
-int a=2,b=3;                //Variable Declarioton
-                            //(removed statement if(b==3) since body is empty
-if ( a == 2 ){              //if statement executing if a==2
-    printf( "*****\n" );}   //added \n to make next print go next line
+int a=2,b=3;                  //Variable Declarioton
+                              //(removed statement if(b==3) since body is empty
+if (( b == 3 )&&( a == 2 )){  //changed if statement condition to (( b == 3 )&&( a == 2 ))
+    printf( "*****\n" );}     //added \n to make next print go next line
 
 else{
-    printf( "-----\n" );}   //added \n to make next print go next line
+    printf( "-----\n" );}     //added \n to make next print go next line
 
-printf( ">>>>>\n" );        //added \n to make next print go next line
-printf( "<<<<<\n" );        //added \n to make next print go next line
+printf( ">>>>>\n" );          //added \n to make next print go next line
+printf( "<<<<<\n" );          //added \n to make next print go next line
 }
