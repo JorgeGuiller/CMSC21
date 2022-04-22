21690133fbf97636e09fb0e437122e09ce13c48c
JorgeGuiller <100853100+JorgeGuiller@users.noreply.github.com>
2022-04-22 14:14:23 +0800

    Update LE1b.c

--- LongExam1/LE1b.c
+++ LongExam1/LE1b.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 void main()
 {
-int a=2,b=3;
+int a=2,b=3;                       //variable declarations
 
-if (( b == 3 )&&( a == 2 )){
+if (( b == 3 )&&( a == 2 )){       //changed if statement condition to (( b == 3 )&&( a == 2 ))
     printf( "*****" );}
 
 else{
