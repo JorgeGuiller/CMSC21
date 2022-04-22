ed447cd5f90a36d64ab9651179ef7e2f2bb1b704
JorgeGuiller <100853100+JorgeGuiller@users.noreply.github.com>
2022-04-22 14:36:30 +0800

    Update LE1(2).c

--- LongExam1/LE1(2).c
+++ LongExam1/LE1(2).c
@@ -1,36 +1,31 @@
 #include<stdio.h>
 void main()
-{ int square,length,width;
-char choice='y';
-while(choice=='y'){
-    printf("Enter square size: ");
-    scanf("%d", &square);
-    for (length=square;length>0;length--){
+{ int square,length,width;                          //variable declarations
+char choice='y';                                    //variable declarations
+ 
+while(choice=='y'){                                 //while loop checks if choice is equal to y
+    printf("Enter square size: ");                  //input indicator
+    scanf("%d", &square);                           //user input
+    for (length=square;length>0;length--){          //length of square loop
 
-        for(width=square;width>0;width--){
+        for(width=square;width>0;width--){          //width of square loop
 
-            if ((length==square)||length==1){
-                printf("* ");
+            if ((length==square)||(length==1)||(width==square)||(width==1)){    //checks if the position is only in the edges
+                printf("* ");                       //added space after * to make the square symmetric
                 }
 
             else{
-                if ((width==square)||width==1){
-                    printf("* ");}
-
-                else{
-                    printf("  ");}
+                printf("  ");}                      //double space to be symmetric
                 }
-            }
-            printf("\n");
+            printf("\n");                           //break line to print on the next line
         }
-    fflush(stdin);
-    do{
-
-    printf("Print another square? Enter y or n:");
-    scanf("%c", &choice);
-    if ((choice!='y')&&(choice!='n')){
-        printf("\nNot a valid choice.\n");
+    fflush(stdin);                                  //clear output buffer
+    do{                                             //do-while loop to repeat progra,
+    printf("Print another square? Enter y or n:");  //input indicator
+    scanf("%c", &choice);                           //input new value for choice
+    if ((choice!='y')&&(choice!='n')){              //if statement to check if input is invalid
+        printf("\nNot a valid choice.\n");          //invalid inpur indicator
         }
-        }while((choice!='y')&&(choice!='n'));
+        }while((choice!='y')&&(choice!='n'));       //do while loop condition, looping until choice equals to y or n
     }
 }
